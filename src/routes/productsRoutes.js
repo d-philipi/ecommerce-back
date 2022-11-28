67d9a9666ad7326dca038c25ebbff9cbb4a368e8
Daniel Philipi <daniphilipi379@gmail.com>
2022-11-28 15:16:11 -0300

    Refact: Routes

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { authValidation } from "../middlewares/authValidationMiddleware.js";
+import { itemCatalog } from "../controllers/itemCatalog.js";
 
 const router = Router();
 
 
 router.get("/products", authValidation, products);
-
+router.get("/home", itemCatalog);
+router.post("/home", itemCatalog);
 
 export default router;
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { signIn, signUp } from "../controllers/userController.js";
-import { itemCatalog, itemCart } from "../controllers/itemCatalog.js";
 import { signInValidation } from "../middlewares/signInValidationMiddleware.js";
 import { signUpValidation } from "../middlewares/signUpValidationMiddleware.js";
 
@@ -10,9 +9,5 @@ const router = Router();
 
 router.post("/sign-up", signUpValidation, signUp);
 router.post("/sign-in", signInValidation, signIn);
-router.get("/home", itemCatalog);
-router.post("/home", itemCatalog);
-router.get("/cart/:email", itemCart);
-router.post("/cart/", itemCart);
 
 export default router;
